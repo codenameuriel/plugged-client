30febc026b40b85fb00ce5411a1daa8054caeb67
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-06-09 00:34:45 -0400

    add logic for conditional rendering in SourceNews component

--- src/components/SourceNews.js
+++ src/components/SourceNews.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import Nav from './Nav'
 import NewsMapper from './NewsMapper'
 
 class SourceNews extends Component {
+  renderDisplay = () => {
+    const { loggedInUser, links, postArticle, sourceNews } = this.props
+    let display;
+    
+    if (loggedInUser.username) {
+      display = 
+        <>
+          <h1>Welcome to the source news</h1>
+          <Nav links={links}/>
+          <NewsMapper news={sourceNews} loggedInUser={loggedInUser} postArticle={postArticle}/>
+        </>
+    } else {
+      display = 
+        <>
+          <h1><Link to="/login">Log in</Link> to view your source news</h1>
+        </>
+    }
+    return display
+  }
+
   render() {
     const { sourceNews,links, loggedInUser, postArticle } = this.props
 
     return (
       <div>
-        <h1>Welcome to the source news</h1>
-        <Nav links={links}/>
-        <NewsMapper news={sourceNews} loggedInUser={loggedInUser} postArticle={postArticle}/>
+        {this.renderDisplay()}
       </div>
     )
   }
