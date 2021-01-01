c256607a888fd17b05e21f120841e197723500ff
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2020-12-31 23:17:27 -0500

    add routes and imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-route-dom';
 
-import Auth from './components/Auth'
-import PageManager from './components/PageManager'
+// import Auth from './components/Auth'
+// import PageManager from './components/PageManager'
 import AppStyles from './styles/App.module.css'
+
+import Login from './components/Login';
+import Signup from './components/Signup';
+import TopNews from './components/TopNews';
+import Dashboard from './components/Dashboard';
+import Collection from './components/CollectionNews'; // change to Collection
+import Category from './components/CategorySelector'; // change to Category
+import Source from './components/SourceNews'; // change to Source
+import Newspaper from './components/NewspaperMenu'; // change to Newspaper
 
 class App extends Component {
   state = {
@@ -160,43 +170,74 @@ class App extends Component {
   }
   
   render() {
-    const links = [
-      <NavLink className={`${AppStyles.link} ${AppStyles.right}`} onClick={this.logOutUser} to="/top-news">LOG OUT</NavLink>,
-      <NavLink className={AppStyles.link} to="/collection">COLLECTION</NavLink>,
-      <NavLink 
-        className={AppStyles.link} 
-        to="/top-news" 
-      >TOP NEWS</NavLink>,
-      <NavLink className={`${AppStyles.link} ${AppStyles.right}`} to="/login">LOG IN</NavLink>,
-      <NavLink className={`${AppStyles.link} ${AppStyles.right}`} to="/signup">SIGN UP</NavLink>,
-      <NavLink className={AppStyles.link} to="/categories">CATEGORIES</NavLink>,
-      <NavLink className={AppStyles.link} to={`/${this.state.loggedInUser.username}/dashboard`}>DASHBOARD</NavLink>,
-      <NavLink className={`${AppStyles.link} ${AppStyles.right}`} to={`/${this.state.loggedInUser.username}/account`}>ACCOUNT</NavLink>,
-      <NavLink  className={AppStyles.link} to="/sources">SOURCES</NavLink>,
-      <NavLink className={AppStyles.link} to="/newspapers">NEWSPAPER</NavLink>
-    ]
+    let routes;
+
+    if (!isAuthenticated) {
+      routes = (
+        <>
+          <Route path="/login" component={Login}/>
+          <Route path="/signup" component={Signup}/>
+          <Route path="/top_news" component={TopNews}/>
+        </>
+      );
+    }
+
+    if (isAuthenticated) {
+      routes = (
+        <>
+          <Route path="/dashboard" component={Dashboard}/>
+          <Route path="/collection" component={Collection}/>
+          <Route path="/categories" component={Category}/>
+          <Route path="/sources" component={Source}/>
+          <Route path="/newspapers" component={Newspaper}/>
+        </>
+      );
+    }
+
+    // const links = [
+    //   <NavLink className={`${AppStyles.link} ${AppStyles.right}`} onClick={this.logOutUser} to="/top-news">LOG OUT</NavLink>,
+    //   <NavLink className={AppStyles.link} to="/collection">COLLECTION</NavLink>,
+    //   <NavLink 
+    //     className={AppStyles.link} 
+    //     to="/top-news" 
+    //   >TOP NEWS</NavLink>,
+    //   <NavLink className={`${AppStyles.link} ${AppStyles.right}`} to="/login">LOG IN</NavLink>,
+    //   <NavLink className={`${AppStyles.link} ${AppStyles.right}`} to="/signup">SIGN UP</NavLink>,
+    //   <NavLink className={AppStyles.link} to="/categories">CATEGORIES</NavLink>,
+    //   <NavLink className={AppStyles.link} to={`/${this.state.loggedInUser.username}/dashboard`}>DASHBOARD</NavLink>,
+    //   <NavLink className={`${AppStyles.link} ${AppStyles.right}`} to={`/${this.state.loggedInUser.username}/account`}>ACCOUNT</NavLink>,
+    //   <NavLink  className={AppStyles.link} to="/sources">SOURCES</NavLink>,
+    //   <NavLink className={AppStyles.link} to="/newspapers">NEWSPAPER</NavLink>
+    // ]
+
+    // return (
+    //   <div>
+    //     <PageManager
+    //        links={links}
+    //        loggedInUser={this.state.loggedInUser}
+    //        postArticle={this.postArticle}
+    //        subscribeToCategory={this.subscribeToCategory}
+    //        loggedInUsersNewspapers={this.state.loggedInUsersNewspapers}
+    //        updateUsersNewspapers={this.updateUsersNewspapers}
+    //        setNewspaper={this.setNewspaper}
+    //        newspaper={this.state.newspaper}
+    //        deleteNewspaper={this.deleteNewspaper}
+    //     />
+    //     <Auth
+    //       links={links}
+    //       loggedInUser={this.state.loggedInUser}
+    //       setLoggedInUser={this.setLoggedInUser}
+    //       unsubscribeFromCategory={this.unsubscribeFromCategory}
+    //       unsubscribeSubmit={this.unsubscribeSubmit}
+    //     />
+    //   </div>
+    // );
 
     return (
-      <div>
-        <PageManager
-           links={links}
-           loggedInUser={this.state.loggedInUser}
-           postArticle={this.postArticle}
-           subscribeToCategory={this.subscribeToCategory}
-           loggedInUsersNewspapers={this.state.loggedInUsersNewspapers}
-           updateUsersNewspapers={this.updateUsersNewspapers}
-           setNewspaper={this.setNewspaper}
-           newspaper={this.state.newspaper}
-           deleteNewspaper={this.deleteNewspaper}
-        />
-        <Auth
-          links={links}
-          loggedInUser={this.state.loggedInUser}
-          setLoggedInUser={this.setLoggedInUser}
-          unsubscribeFromCategory={this.unsubscribeFromCategory}
-          unsubscribeSubmit={this.unsubscribeSubmit}
-        />
-      </div>
+      <Switch>
+        {routes}
+        <Redirect exact from="/" to="/top_news"/>
+      </Switch>
     );
   }
 }
