c37b160016f369501451e34ba46c519985058ba0
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2021-01-14 14:57:09 -0500

    create DrawerToggle component

---
+++ src/components/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import DrawerToggleStyles from './DrawerToggle.module.css';
+
+const DrawerToggle = props => {
+  return (
+    <div className={DrawerToggleStyles.DrawerToggle}>
+      
+    </div>
+  );
+};
+
+export default DrawerToggle;
