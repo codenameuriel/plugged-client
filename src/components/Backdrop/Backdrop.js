65bbdc5ddcbf219d897f6caab568d22419b2defb
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2021-01-14 15:21:45 -0500

    create Backdrop component and styling

---
+++ src/components/Backdrop/Backdrop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BackdropStyles from './Backdrop.module.css';
+
+const BackDrop = () => {
+  return (
+    <div className={BackdropStyles.Backdrop}>
+      
+    </div>
+  );
+};
+
+export default BackDrop;
---
+++ src/containers/SideDrawer/SideDrawer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SideDrawerStyles from './SideDrawer.moduel.css';
+
+const SideDrawer = () => {
+  return (
+    <div className={SideDrawerStyles.SideDrawer}>
+      
+    </div>
+  );
+};
+
+export default SideDrawer;
