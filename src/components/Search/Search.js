fc218ab280fd6019322e7d0c7e7dffedc7dfee90
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2021-01-21 14:35:02 -0500

    add input handling for Search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Button from '../UI/Button/Button';
 
 import SearchStyles from './Search.module.css';
 
-const Search = props => {
-  return (
-    <div className={SearchStyles.Search}>
-      <input type="text" placeholder="Search by topic" />
-      <Button type={"search"} onClick={null} description={"Search"} />
-    </div>
-  );
-};
+// const search = <input className={DashboardStyles.search} onChange={handleSearchChange} type="text" placeholder="Search news by topic" value={searchTopic} />
+    // const searchBtn = <button className={DashboardStyles.submitBtn} onClick={() => {
+    //   getTopicNews()
+    //   history.push("/dashboard/topic-news")
+    // }}>Search</button>
+
+class Search extends Component {
+  state = {
+    inputValue: ""
+  }
+
+  inputChange = event => {
+    this.setState({ inputValue: event.target.value });
+  }
+
+  render() {
+    const { inputValue } = this.state;
+    return (
+      <div className={SearchStyles.Search}>
+        <input 
+          type="text" 
+          value={inputValue} 
+          onChange={this.inputChange}
+          placeholder="Search by topic" />
+        <Button type={"search"} onClick={null} description={"Search"} />
+      </div>
+    );
+  }
+}
 
 export default Search;
