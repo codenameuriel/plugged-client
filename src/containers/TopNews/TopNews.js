c9692ea5866ddae1b8a386a65dc64f9380adcaa7
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2021-01-24 22:04:37 -0500

    remove comments from TopNews component

--- src/containers/TopNews/TopNews.js
+++ src/containers/TopNews/TopNews.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actionCreators from '../../store/actions/index';
-// import Nav from '../../components/Nav.js'
-// import NewsMapper from '../../components/NewsMapper'
 
 import Layout from '../../hoc/Layout/Layout';
 import Content from '../../components/Content/Content';
@@ -19,59 +17,7 @@ class TopNews extends Component {
     this.props.onGetTopNews();
   }
 
-  // componentDidUpdate(prevProps) {
-  //   if (prevProps.page !== this.props.page)
-  //     // this.getArticlesFromDB()
-  //     this.getTopNews()
-  // }
-
-  // getTopNews = () => {
-  //   const { page } = this.state;
-    
-  //   fetch(`https://newsapi.org/v2/top-headlines?country=us&pageSize=9&page=${page}`, apiKey)
-  //     .then(resp => resp.json())
-  //     .then(data => this.setState({
-  //        topNews: data.articles 
-  //      }, () => setTotalResults(data.totalResults)))
-  // }
-
   render() {
-    // const { 
-    //   page, loggedInUser, showPrevPageButton, prevPage, nextPage, lastPage
-    // } = this.props
-
-    // let jumbotronMessage; 
-    // let nextPageInnerText = `Go to Page ${page + 1}`
-
-    // if (!loggedInUser.username) {
-    //   jumbotronMessage = 'Log in or sign up to get your news'
-    // } else if (loggedInUser.username) {
-    //   jumbotronMessage = 'Live and breaking headlines'
-    // }
-
-    // if (lastPage) {
-    //   nextPageInnerText = 'Back to Page 1'
-    // }
-   
-    {/* <header className={TopNewsStyles.header} >
-      <h1>Top Headlines</h1>
-      <p>{jumbotronMessage}</p>
-    </header>
-  <Nav 
-    links={this.props.links}
-  />
-  {showPrevPageButton && 
-    <button className={TopNewsStyles.button} onClick={prevPage} >Previous Page</button>
-  }
-  <button
-    className={TopNewsStyles.button} 
-    onClick={nextPage} >{nextPageInnerText}</button>
-  <NewsMapper
-    news={this.state.topNews}
-    loggedInUser={this.props.loggedInUser}
-    postArticle={this.props.postArticle}
-  /> */}
-
     const { type, title, subtitle } = this.state;
     return (
       <Layout title={title} subtitle={subtitle} type={type}>
