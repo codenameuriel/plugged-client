1159b210c6a111b224ebe7e924f7f9129beba59c
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2021-02-18 20:19:09 -0500

    reformat Button component

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,25 +1,30 @@
-import React from 'react';
-import ButtonStyles from './Button.module.css';
+/** @format */
+
+import React from 'react'
+import ButtonStyles from './Button.module.css'
 
 const Button = ({ type, description, onClick }) => {
   // onClick prop will contain click event
   let buttonStyles = [ButtonStyles.Button]
   switch (type) {
-    case "collection":
-      buttonStyles = [ButtonStyles.Button, ButtonStyles.Collection];
-      break;
-    case "pageManager":
-      buttonStyles = [ButtonStyles.Button, ButtonStyles.PageManager];
-      break;
-    case "search":
-      buttonStyles = [ButtonStyles.Button, ButtonStyles.Search];
-      break;
-    default: break;
+    case 'collection':
+      buttonStyles = [ButtonStyles.Button, ButtonStyles.Collection]
+      break
+    case 'pageManager':
+      buttonStyles = [ButtonStyles.Button, ButtonStyles.PageManager]
+      break
+    case 'search':
+      buttonStyles = [ButtonStyles.Button, ButtonStyles.Search]
+      break
+    default:
+      break
   }
 
   return (
-    <button className={buttonStyles.join(" ")} onClick={onClick}>{description}</button>
-  );
-};
+    <button className={buttonStyles.join(' ')} onClick={onClick}>
+      {description}
+    </button>
+  )
+}
 
-export default Button;
+export default Button
