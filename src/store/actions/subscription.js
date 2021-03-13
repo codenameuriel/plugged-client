e2b2ca42f4e9ef9186268ea856138c0e528f19a6
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2021-03-13 11:33:06 -0500

    change categories subscription url

--- src/store/actions/subscription.js
+++ src/store/actions/subscription.js
@@ -4,15 +4,18 @@ import { postData } from '../../utils/fetch';
 export const subscribeToCategories = (isSubscribed, categories) => {
   return async (dispatch, getState) => {
     const { user } = getState().auth;
+
+    // format categories for backend
+    const formattedCategories = categories.split(',');
     const subscriptionData = {
       username: user.username,
-      categories,
+      categories: formattedCategories,
       isSubscribed // backend will use isSubscribed to determine action to take with categories
     };
 
     try {
-      // destructure for categories array
-      const { categories: updatedCategories} = await postData('/subscriptions/categories', subscriptionData);
+      // updated array of user's categories
+      const updatedCategories = await postData('/subscriptions/categories', subscriptionData);
 
       dispatch(updateUsersCategories(updatedCategories));
     } catch (error) {
