ea5a66a2bf76e644501e298f96ae05f9152b1e46
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2021-04-03 12:07:35 -0400

    write logic for Newspaper form and render Newspaper form

--- src/containers/Newspaper/NewspaperForm/NewspaperForm.js
+++ src/containers/Newspaper/NewspaperForm/NewspaperForm.js
@@ -1,19 +1,54 @@
 import React from 'react';
 
+import { CATEGORIES } from '../../../utils/categories';
+
 class NewspaperForm extends React.Component {
   state = {
     newspaperTitle: ''
   }
 
   renderCategorySelection() {
-
     return (
       <fieldset onChange={null}>
-
+        <label htmlFor="categories">
+          Select Categories:
+          <br />
+          {CATEGORIES.map(category => {
+            const { type } = category;
+            return (
+              <>
+                <label htmlFor="categories">
+                  {type}:
+                  <input 
+                    type="checkbox"
+                    name="categories"
+                    value={type} />
+                </label>
+                <br />
+              </>
+            );
+          })}
+        </label>
       </fieldset>
     );
   }
 
+  renderSourcesSelection() {
+    return (
+      <label htmlFor="sources">
+        Select Sources by Category:
+        <select name="sources" onChange={null}>
+          <option value="Select">Select Category</option>
+          {CATEGORIES.map(category => {
+            const { type } = category;
+            return (
+              <option value={type}>{type}</option>
+            );
+          })}
+        </select>
+      </label>
+    );
+  }
 
   render() {
     const { newspaperTitle } = this.state;
@@ -31,6 +66,8 @@ class NewspaperForm extends React.Component {
         </label>
         <br />
         {this.renderCategorySelection()}
+        <br />
+        {this.renderSourcesSelection()}
       </form>
     );
   }
---
+++ src/containers/Newspaper/NewspaperMenu/NewspaperMenu.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import NewspaperForm from '../NewspaperForm/NewspaperForm';
+
+class NewspaperMenu extends React.Component {
+  state = {
+    
+  }
+
+  render() {
+    return <NewspaperForm />;
+  }
+}
+
+export default NewspaperMenu;
