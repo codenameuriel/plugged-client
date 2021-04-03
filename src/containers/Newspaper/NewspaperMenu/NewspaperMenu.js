b2c81213b79600a4a2f37a005a52d92911a50d74
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2021-04-03 12:39:04 -0400

    add newspaper form toggle

--- src/containers/Newspaper/NewspaperMenu/NewspaperMenu.js
+++ src/containers/Newspaper/NewspaperMenu/NewspaperMenu.js
@@ -1,20 +1,27 @@
 import React from 'react';
 
 import Layout from '../../../hoc/Layout/Layout';
+import Button from '../../../components/UI/Button/Button';
 import NewspaperForm from '../NewspaperForm/NewspaperForm';
 
 class NewspaperMenu extends React.Component {
   state = {
     type: 'newspaper-menu',
     title: 'Newspaper Menu',
-    subtitle: 'Create and customize your news feed'
+    subtitle: 'Create and customize your news feed',
+    viewForm: false
+  }
+
+  handleBtnClick = () => {
+    this.setState(prevState => ({ viewForm: !prevState.viewForm }));
   }
 
   render() {
-    const { title, subtitle, type } = this.state;
+    const { title, subtitle, type, viewForm } = this.state;
     return (
       <Layout title={title} subtitle={subtitle} type={type}>
-        <NewspaperForm />
+        <Button onClick={this.handleBtnClick} description={viewForm ? 'Close' : 'Build a newspaper'} />
+        {viewForm && <NewspaperForm />}
       </Layout>
     );
   }
