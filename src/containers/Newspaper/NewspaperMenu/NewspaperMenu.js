ff00ccf91b08b622a68f300541bfb9a747e4ecb7
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2021-04-10 16:38:27 -0400

    create NewspaperCard to render newspapers

--- src/containers/Newspaper/NewspaperMenu/NewspaperMenu.js
+++ src/containers/Newspaper/NewspaperMenu/NewspaperMenu.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import Layout from '../../../hoc/Layout/Layout';
 import Button from '../../../components/UI/Button/Button';
 import NewspaperForm from '../NewspaperForm/NewspaperForm';
+import NewspaperCard from '../NewspaperCard/NewspaperCard';
 
 class NewspaperMenu extends React.Component {
   state = {
@@ -16,15 +18,31 @@ class NewspaperMenu extends React.Component {
     this.setState(prevState => ({ viewForm: !prevState.viewForm }));
   }
 
+  renderNewspapers() {
+    const { newspapers } = this.props;
+    return (
+      <section>
+        {newspapers.map(newspaper => <NewspaperCard newspaper={newspaper} />)}
+      </section>
+    );
+  }
+
   render() {
     const { title, subtitle, type, viewForm } = this.state;
     return (
       <Layout title={title} subtitle={subtitle} type={type}>
         <Button onClick={this.toggleFormView} description={viewForm ? 'Close' : 'Build a newspaper'} />
         {viewForm && <NewspaperForm />}
+        {this.renderNewspapers()}
       </Layout>
     );
   }
 }
 
-export default NewspaperMenu;
+const mapStateToProps = state => {
+  return {
+    newspapers: state.newspaper.newspapers
+  };
+};
+
+export default connect(mapStateToProps)(NewspaperMenu);
