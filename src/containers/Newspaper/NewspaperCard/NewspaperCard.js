2b8115ec39bb5a637da2516c64fe0e12d73a263c
Uriel Rodriguez <u.e.rodriguez@gmail.com>
2021-04-12 16:14:24 -0400

    add NewspaperCard styling

--- src/containers/Newspaper/NewspaperCard/NewspaperCard.js
+++ src/containers/Newspaper/NewspaperCard/NewspaperCard.js
@@ -1,30 +1,33 @@
 import React from 'react';
+import NewspaperCardStyles from './NewspaperCard.module.css';
 
 const NewspaperCard = props => {
   // extract newspaper data
   const { newspaper: { title, categories, sources, topics }} = props;
 
   // render newspaper data
-  // const renderNewspaperSection = newspaperData => {
-  //   // newspaperData will be represented as an array e.g. array of categories
-  //   return (
-  //     <section>
-  //       {newspaperData.map(section => {
-  //         return <p>{section}</p>;
-  //       })}
-  //     </section>
-  //   );
-  // };
+  const renderNewspaperSection = (newspaperData, property) => {
+    // newspaperData will be represented as an array e.g. array of categories
+    return (
+      <section>
+        {newspaperData.map(section => {
+          return (
+            <p className={NewspaperCardStyles.Name}>{section[property]}</p>
+          );
+        })}
+      </section>
+    );
+  };
   
   return (
-    <div>
+    <div className={NewspaperCardStyles.NewspaperCard}>
       <h1>The {title} Newspaper</h1>
-      {/* <h2>Categories</h2>
-      {renderNewspaperSection(categories)}
+      <h2>Categories</h2>
+      {renderNewspaperSection(categories, 'category')}
       <h2>Sources</h2>
-      {renderNewspaperSection(sources)}
+      {renderNewspaperSection(sources, 'source')}
       <h2>Topics</h2>
-      {renderNewspaperSection(topics)} */}
+      {renderNewspaperSection(topics, 'topic')}
     </div>
   );
 };
